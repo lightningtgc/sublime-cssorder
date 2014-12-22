e3f6ab64a7a5d78beb3875fbf84b5b9f1d63285e
lightningtgc <lightningtgc@gmail.com>
2014-12-22 11:21:05 +0800

    Handle compressive file case

--- cssorder.js
+++ cssorder.js
@@ -21,7 +21,6 @@ function preHandleSrc (cssSrc) {
    * .a { background: url("http://www.qq.com/one.png");} 
    *
    */
-
   cssSrc = cssSrc.replace(/:\/\//g, '#tidy2#');
 
   /*
@@ -29,13 +28,12 @@ function preHandleSrc (cssSrc) {
    * .a { background: url("data:image/png;base64,iVBOMVEX///////////////+g0jAqu8zdII=");} 
    *
    */
-  cssSrc = cssSrc.replace(/(?:data)(.*)(?=\))/g, function(match){
+  cssSrc = cssSrc.replace(/data[\s\S]+?\)/g, function(match){
       match = match.replace(/:/g, '#tidy3#');
       match = match.replace(/;/g, '#tidy4#');
       match = match.replace(/\//g, '#tidy5#');
       return match;
-      });
-
+  });
   /*
    * fix multiple line comment include single comment //
    * eg: / * sth // another * /
@@ -44,40 +42,52 @@ function preHandleSrc (cssSrc) {
       // handle single comment //
       match = match.replace(/\/\//g, '#tidy6#');
       return  match;
-      });
+  });
 
   /*
    * fix single comment like:  // something 
    * It can't works in IE, and will cause parse error
-   *
+   * update: handle single line for compressive case
    */
   cssSrc = cssSrc.replace(/(^|[^:|'|"|\(])\/\/.+?(?=\n|\r|$)/g, function(match){
-      var targetMatch;
-      //handle first line
-      if (match.charAt(0) !== '/' ) {
-      // remove first string and / and \ 
-      targetMatch = match.substr(1).replace(/\\|\//g, '');;
-      return match.charAt(0) + '/*' + targetMatch + '*/';
-      } else {
-      targetMatch = match.replace(/\\|\//g, '');
-      return '/*' + targetMatch + '*/';
-      }
-      });
 
+      // Handle compressive file
+      if (match.indexOf('{') === -1 || match.indexOf('}') === -1) {
+          var targetMatch;
+
+          //handle first line
+          if (match.charAt(0) !== '/' ) {
+              // remove first string and / and \ 
+              targetMatch = match.substr(1).replace(/\\|\//g, '');;
+              return match.charAt(0) + '/*' + targetMatch + '*/';
+          } else {
+              targetMatch = match.replace(/\\|\//g, '');
+              return '/*' + targetMatch + '*/';
+          }
+      } else {
+          throw new Error('There maybe some single comment // in this file.');
+      }
+  });
   return cssSrc;
 }
 
 // after handle src, recover special string
 function afterHandleSrc (content) {
 
-  content = content.replace(/#tidy1#/g, 'progid:');
-  content = content.replace(/#tidy2#/g, '://');
-  content = content.replace(/#tidy3#/g, ':');
-  content = content.replace(/#tidy4#/g, ';');
-  content = content.replace(/#tidy5#/g, '/');
-  content = content.replace(/#tidy6#/g, '//');
+    content = content.replace(/#tidy1#/g, 'progid:');
+    content = content.replace(/#tidy2#/g, '://');
+    content = content.replace(/#tidy3#/g, ':');
+    content = content.replace(/#tidy4#/g, ';');
+    content = content.replace(/#tidy5#/g, '/');
+    content = content.replace(/#tidy6#/g, '//');
 
-  return content;
+    // handle compressive css file 
+    content = content.replace(/(\}|\*\/)(?!\r|\n)/g, function(match){
+        // \n will translate to \r\n in windows?
+        return match + '\n';
+    });
+
+    return content;
 }
 
 process.stdin.on('end', function () {
